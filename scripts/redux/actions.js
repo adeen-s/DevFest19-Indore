adeen-s/DevFest19-Indore

commit 228f2bc6f630aa15ab11bf24169ee8dca937fe2e

made the team members appear in alphabetical order

scripts/redux/actions.js
@@ -529,6 +529,7 @@ const galleryActions = {
 
 const _getTeamMembers = (teamId) => firebase.firestore()
   .collection('team').doc(teamId).collection('members')
+  .orderBy("name")
   .get()
   .then((snaps) => snaps.docs
     .map((snap) => Object.assign({}, snap.data(), { id: snap.id }))

commit fba6f29f6171681367e7c6444aa209fec68ae98a

Fix lint error introduced in 228f2bc6

scripts/redux/actions.js
@@ -529,7 +529,7 @@ const galleryActions = {
 
 const _getTeamMembers = (teamId) => firebase.firestore()
   .collection('team').doc(teamId).collection('members')
-  .orderBy("name")
+  .orderBy('name')
   .get()
   .then((snaps) => snaps.docs
     .map((snap) => Object.assign({}, snap.data(), { id: snap.id }))
@@ -881,4 +881,3 @@ const helperActions = {
     }
   },
 };
-